ba3e1ef1b768d8130e634341a830d4ad8aecc0a1
manee2k6 <manee2k6@gmail.com>
2017-08-29 18:54:07 -0700

    Add files via upload

---
+++ src/com/selecon/utils/AutoConstants.java
@@ -0,0 +1,23 @@
+package com.selecon.utils;
+
+public class AutoConstants {
+	
+	public class Url{
+		public static final String URL="URL";
+		public static final String SELENIUM_URL="URLs";
+		public static final String PROJECT_URL="ProjectURL";
+		public static final String DOWNLOAD_URL="DownloadURL";
+	}
+	
+	public class Credentials{
+		public static final String USERNAME="username";
+		public static final String PASSWORD="pwd";
+	}
+	
+	public class BrowserKey{
+		public static final String FIREFOX_KEY="firefoxKey";
+		public static final String CHROME_KEY="chromeKey";
+		public static final String IE_KEY="ieKey";
+	}
+
+}
